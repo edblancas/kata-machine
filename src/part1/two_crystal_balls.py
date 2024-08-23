a8da694691988cab95c23b5ea76431c60c7115fd
Daniel Blancas <edblancas@gmail.com>
2024-08-22 21:47:07 -0600

    add crystall balls sol

--- src/part1/two_crystal_balls.py
+++ src/part1/two_crystal_balls.py
@@ -1,3 +1,22 @@
-def two_crystal_balls():
-    return 
+# Given two crystal balls that will break if dropped form high enough distance,
+# determine the exact spot in which it will break in the most optimized way.
+import math
 
+# time complexity O(sqrt(n)), n = len(breaks)
+def two_crystal_balls(breaks: list[bool]) -> int:
+    if breaks[0]:
+        return 0
+
+    jump = math.floor(math.sqrt(len(breaks)))
+
+    for jump in range(jump, len(breaks), jump):
+        if breaks[jump]:
+            break
+
+    jump -= jump
+
+    for step in range(jump, len(breaks)):
+        if breaks[step]:
+            return step
+
+    return -1
