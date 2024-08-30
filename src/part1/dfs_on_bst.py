369aa0932b5e675ee56c862f7e14d1e7661f5cd1
Daniel Blancas <edblancas@gmail.com>
2024-08-30 08:50:52 -0600

    fix bst search and comments

--- src/part1/dfs_on_bst.py
+++ src/part1/dfs_on_bst.py
@@ -9,15 +9,20 @@ def dfs(head: BinaryNode, needle: int) -> bool:
     return search(head, needle)
 
 def search(head: BinaryNode | None, needle: int) -> bool:
-    if head is None:
+    if not head:
         return False
 
     if head.value == needle:
         return True
 
+    # we dont need both ifs
+    # also the out is wrong cuz we are searching both side
+    # the whole point is just search one side
+    # if needle <= head.value:
+        # out = search(head.left, needle)
+    # elif needle > head.value:
+        # out = search(head.right, needle)
     if needle <= head.value:
-        out = search(head.left, needle)
-    elif needle > head.value:
-        out = search(head.right, needle)
+        return search(head.left, needle)
 
-    return out
+    return search(head.right, needle)
