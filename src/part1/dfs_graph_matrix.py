a0325df77248f04899983d0ccd4acb5eb1a32207
Daniel Blancas <edblancas@gmail.com>
2024-09-06 11:18:05 -0600

    dfs recursive with adjacency matrix

--- src/part1/dfs_graph_matrix.py
+++ src/part1/dfs_graph_matrix.py
@@ -1,4 +1,24 @@
-from src.util.graph import WeightedAdjacencyList
+# the easiest way is to convert the matrix to a adj list
+# but can be done like in the maze solver
+def dfs(graph: list[list[int]], source: int, needle: int) -> list[int] | None:
+    processed = set()
+    path = []
 
-def dfs(graph: WeightedAdjacencyList, source: int, needle: int) -> list[int] | None:
-    pass
+    def dfs_rec(curr: int):
+        if curr == needle:
+            path.append(curr)
+            return True
+        if curr in processed:
+            return False
+        processed.add(curr)
+        path.append(curr)
+        for neighbor, weight in enumerate(graph[curr]):
+            # improvement so dont go one more level deep in the call stack
+            # and return until the base cases
+            if neighbor in processed: continue
+            if weight > 0 and dfs_rec(neighbor):
+                return True
+        path.pop()
+
+    dfs_rec(source)
+    return path or None
