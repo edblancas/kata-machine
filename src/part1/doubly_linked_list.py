676a93098e4e36fc072d7b4b6f67d570330a9d4a
Daniel Blancas <edblancas@gmail.com>
2025-02-07 00:21:20 -0600

    7 - double linked list

--- src/part1/doubly_linked_list.py
+++ src/part1/doubly_linked_list.py
@@ -11,15 +11,102 @@ from src.util.linked_list import ListInterface, ListNode
 class DoublyLinkedList(ListInterface):
     def __init__(self, length=0):
         super().__init__(length)
+        self.length = length
+        self.head = None
 
-    def insert_at(self, item: int, idx: int) -> None: ...
+    def insert_at(self, item: int, idx: int) -> None:
+        if idx < 0:
+            return None
 
-    def remove(self, item: int) -> int | None: ...
+        curr = self.head
+        for _ in range(idx):
+            curr = curr.next
 
-    def remove_at(self, idx: int) -> ListNode | None: ...
+        node = ListNode(item)
 
-    def get(self, idx): ...
+        if idx == 0:
+            self.head = node
 
-    def prepend(self, item): ...
+        if curr.prev:
+            curr.prev.next = node
+        if curr.next:
+            curr.next.prev = node
+            curr.next = curr.next.next
+        node.next = curr
+        curr.prev = node
+        self.length += 1
 
-    def append(self, item): ...
+
+    def remove(self, item: int) -> int | None:
+        if self.length == 0:
+            return None
+
+        curr = self.head
+        i = 0
+        while curr:
+            if curr.value == item:
+                return self.remove_at(i)
+            curr = curr.next
+
+        return None
+
+
+    def remove_at(self, idx: int) -> ListNode | None:
+        if idx < 0 or idx >= self.length or self.length == 0:
+            return None
+
+        if idx == 0:
+            value = self.head.value
+            self.head = self.head.next
+            self.length -= 1
+            return value
+
+        curr = self.head
+        for _ in range(idx):
+            curr = curr.next
+
+        curr.prev.next = curr.next
+        if curr.next:
+            curr.next.prev = curr.prev
+        self.length -= 1
+        return curr.value
+
+
+
+    def get(self, idx):
+        if idx < 0 or idx >= self.length:
+            return None
+
+        curr = self.head
+        for _ in range(idx):
+            curr = curr.next
+
+        return curr.value
+
+
+    def prepend(self, item):
+        node = ListNode(item)
+        if self.length == 0:
+            self.head = node
+            self.length += 1
+            return
+
+        self.head.prev = node
+        node.next = self.head
+        self.head = node
+        self.length += 1
+
+    def append(self, item):
+        node = ListNode(item)
+        if self.length == 0:
+            self.head = node
+            self.length += 1
+            return
+
+        curr = self.head
+        while curr.next:
+            curr = curr.next
+
+        curr.next = node
+        node.prev = curr
+        self.length += 1
--- test/util/test_list.py
+++ test/util/test_list.py
@@ -22,8 +22,13 @@ def test_list(self, list: ListInterface[int]) -> None:
 
     self.assertEqual(list.get(2), 5)
     self.assertEqual(list.get(0), 9)
-    list.print()
     self.assertEqual(list.remove(9), 9)
-    list.print()
     self.assertEqual(list.length, 2)
     self.assertEqual(list.get(0), 7)
+
+
+def test_insert_at_double_linked_list(self, list: ListInterface[int]) -> None:
+    test_list(self, list)
+    list.insert_at(66, 1)
+    self.assertEqual(list.get(1), 66)
+    self.assertEqual(list.get(2), 5)
