ced5eba68aa84e679f53ee4567797f55b7d04518
Daniel Blancas <edblancas@gmail.com>
2025-02-06 20:46:43 -0600

    2 - binary search

--- src/part1/binary_search_list.py
+++ src/part1/binary_search_list.py
@@ -1,1 +1,16 @@
-def binary_search(haystack: list[int], needle: int) -> bool: ...
+def binary_search(haystack: list[int], needle: int) -> bool:
+    hi = len(haystack) - 1
+    lo = 0
+
+    while lo <= hi:
+        # m = (lo + hi) // 2
+        m = lo + (hi - lo) // 2
+        v = haystack[m]
+        if v == needle:
+            return True
+        elif needle < v:
+            hi = m - 1
+        else:
+            lo = m + 1
+
+    return False
