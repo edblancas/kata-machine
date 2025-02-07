f67ffe2987d1f20d4ef862b7e10497cac01de229
Daniel Blancas <edblancas@gmail.com>
2025-02-06 21:12:50 -0600

    3 - crytal balls

--- src/part1/two_crystal_balls.py
+++ src/part1/two_crystal_balls.py
@@ -1,1 +1,19 @@
-def two_crystal_balls(breaks: list[bool]) -> int: ...
+import math
+
+def two_crystal_balls(breaks: list[bool]) -> int:
+    step = math.floor(math.sqrt(len(breaks)))
+
+    for i in range(0, len(breaks), step):
+        if breaks[i] == True:
+            break
+
+    print('first break', i)
+    i -= step
+    print('go back step to', i)
+
+    for j in range(i, len(breaks)):
+        if breaks[j] == True:
+            print('second crystal break, first breat at', j)
+            return j
+
+    return -1
