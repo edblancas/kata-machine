5858263780972a1da8099da45fb3af0b49570c58
Daniel Blancas <edblancas@gmail.com>
2025-02-06 22:23:34 -0600

    5 - queue

--- src/part1/queue.py
+++ src/part1/queue.py
@@ -3,10 +3,34 @@ from src.util.linked_list import ListNode
 
 
 class Queue:
-    def __init__(self, length=0): ...
+    def __init__(self, length=0):
+        self.length = length
+        self.head = None
+        self.tail = None
 
-    def enqueue(self, item): ...
+    def enqueue(self, item):
+        node = ListNode(item)
 
-    def deque(self): ...
+        if self.length == 0:
+            self.head = node
+            self.tail = node
+            self.length += 1
+            return
 
-    def peek(self): ...
+        self.tail.next = node
+        self.tail = node
+        self.length += 1
+
+    def deque(self):
+        if self.length == 0:
+            return None
+
+        value = self.head.value
+        self.head = self.head.next
+        self.length -= 1
+        return value
+
+    def peek(self):
+        if self.length == 0:
+            return None
+        return self.head.value
