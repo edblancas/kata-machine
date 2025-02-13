80c1d98733c89611623e095fb83e896f8f6b9f0e
Daniel Blancas <edblancas@gmail.com>
2025-02-13 08:31:53 -0600

    13 - compare bin trees

--- src/part1/compare_binary_trees.py
+++ src/part1/compare_binary_trees.py
@@ -3,4 +3,15 @@
 
 from src.util.local_types import BinaryNode
 
-def compare_binary_trees(head: BinaryNode, head2: BinaryNode) -> bool: ...
+def compare_binary_trees(head: BinaryNode, head2: BinaryNode) -> bool:
+    if head == None and head2 == None:
+        return True
+
+    if head == None or head2 == None:
+        return False
+
+    if head.value != head.value:
+        return False
+
+    return compare_binary_trees(head.left, head2.left) and \
+        compare_binary_trees(head.right, head2.right)
