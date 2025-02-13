04a6d478ef41a456f254d66311ba6bb1151fa388
Daniel Blancas <edblancas@gmail.com>
2025-02-13 09:52:33 -0600

    14 - bin tree breath first search

--- src/part1/btbfs.py
+++ src/part1/btbfs.py
@@ -2,6 +2,30 @@
 from src.util.local_types import BinaryNode
 from collections import deque
 
-def bfs(head: BinaryNode) -> list[int]: ...
+def bfs(head: BinaryNode) -> list[int]:
+    q = deque([head])
+    path = []
 
-def bfs_find(head: BinaryNode, needle: int) -> bool: ...
+    while q:
+        next = q.popleft()
+
+        path.append(next.value)
+
+        if next.left: q.append(next.left)
+        if next.right: q.append(next.right)
+
+    return path
+
+
+def bfs_find(head: BinaryNode, needle: int) -> bool:
+    q = deque([head])
+    
+    while q:
+        next = q.popleft()
+        if not next: continue
+
+        if next.value == needle: return True
+        q.append(next.left)
+        q.append(next.right)
+
+    return False
