eec46129c2fb99e2d2777f74ec88ff85f311bef0
Daniel Blancas <edblancas@gmail.com>
2025-02-14 17:39:16 -0600

    19 - dfs recursive with an adjacency list

--- src/part1/dfs_graph_list.py
+++ src/part1/dfs_graph_list.py
@@ -1,3 +1,26 @@
 from src.util.graph import GraphEdge, WeightedAdjacencyList
 
-def dfs(graph: WeightedAdjacencyList, source: int, needle: int) -> list[int] | None: ...
+def dfs(graph: WeightedAdjacencyList, source: int, needle: int) -> list[int] | None:
+    path = []
+    visited = set()
+
+    def walk(curr):
+        if curr == needle:
+            path.append(curr)
+            return True
+        if curr in visited:
+            return False
+
+        visited.add(curr)
+        path.append(curr)
+
+        adj_to_curr = graph[curr]
+        for next in adj_to_curr:
+            if walk(next.to):
+                return True
+
+        path.pop()
+        return False
+
+    walk(source)
+    return path or None
