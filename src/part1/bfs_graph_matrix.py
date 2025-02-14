d79f8c675358837b7f7570b9f98f85e5ffa51919
Daniel Blancas <edblancas@gmail.com>
2025-02-14 17:04:32 -0600

    18 - bfs with a graph matrix

--- src/part1/bfs_graph_matrix.py
+++ src/part1/bfs_graph_matrix.py
@@ -1,4 +1,33 @@
 # return the path that we took to the needle node or None if not found
 from collections import deque
 
-def bfs(adj_matrix: list[list[int]], source: int, needle: int) -> list[int] | None: ...
+def bfs(adj_matrix: list[list[int]], source: int, needle: int) -> list[int] | None:
+    q = deque([source])
+    visited = set()
+    prev = dict()
+
+    while q:
+        curr = q.popleft()
+        visited.add(curr)
+        if curr == needle:
+            break
+        for next, weight in enumerate(adj_matrix[curr]):
+            if not weight:
+                continue
+            if next in visited:
+                continue
+            prev[next] = curr
+            q.append(next)
+
+    if not prev.get(needle):
+        return None
+
+    curr = needle
+    path = deque()
+    while prev.get(curr) != None:
+        path.appendleft(curr)
+        curr = prev[curr]
+
+    path.appendleft(source)
+
+    return list(path)
