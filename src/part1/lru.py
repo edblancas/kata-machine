c27de0447f250024a74c4a4710dd0270b33cf585
Daniel Blancas <edblancas@gmail.com>
2025-02-15 00:07:56 -0600

    add lru skeleton without lookup

--- src/part1/lru.py
+++ src/part1/lru.py
@@ -10,7 +10,7 @@ class LRUNode[K, V]:
     prev: LRUNode[K, V] | None = None
 
 
-class LRU[K, V]:
+class LRUNoLookup[K, V]:
     def __init__(self, capacity=3):
         self.capacity = capacity
 
@@ -19,5 +19,16 @@ class LRU[K, V]:
     def get(self, key: K) -> V | None: ...
 
 
-class LRUReverseLookup[K,V]:
-    ...
+@dataclass
+class Node[V]:
+    value: V 
+    next: LRUNode[V] | None = None
+    prev: LRUNode[V] | None = None
+
+class LRU[K, V]:
+    def __init__(self, capacity=3):
+        self.capacity = capacity
+
+    def update(self, key: K, value: V) -> None: ...
+
+    def get(self, key: K) -> V | None: ...
