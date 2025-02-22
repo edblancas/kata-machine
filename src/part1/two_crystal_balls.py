1818bb72ecd6869a7a1d6e2709af8b532578ee26
Daniel Blancas <edblancas@gmail.com>
2025-02-22 12:15:02 -0600

    add description to two crystal balls

--- src/part1/two_crystal_balls.py
+++ src/part1/two_crystal_balls.py
@@ -1,1 +1,4 @@
+# Given two crystal balls that will break if dropped form high enough distance,
+# determine the exact spot in which it will break in the most optimized way.
+
 def two_crystal_balls(breaks: list[bool]) -> int: ...
