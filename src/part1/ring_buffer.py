1c63833bfb6cb90030e904bab6c27d0bcaab5cc7
Daniel Blancas <edblancas@gmail.com>
2025-02-22 18:06:12 -0600

    fix ring buffer queue implementation and tests, add desc

--- src/part1/ring_buffer.py
+++ src/part1/ring_buffer.py
@@ -1,5 +1,8 @@
 # implement a ring buffer with an array
 # i.e. a queue implemented with an array, enqueue to the tail, deque from the head
+# note: just the enqueue and deque, and size should be checked in the tests.
+#       but for better implementation purposes, the tail is checked and should be
+#       point to the next position to enqueue
 
 
 class RingBuffer:
