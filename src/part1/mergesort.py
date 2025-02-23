c782698cc756411e6014623616ebe15fd35d09e7
Daniel Blancas <edblancas@gmail.com>
2025-02-23 08:34:33 -0600

    add merge sort iterative by chatgpt

--- src/part1/mergesort.py
+++ src/part1/mergesort.py
@@ -33,6 +33,29 @@ def mergesort_rec(arr):
         return []
     return ms(0, len(arr) - 1)
 
-# to complicated
+# modifies the current array and also return it
 def mergesort_iter(arr):
-    pass
+    def merge(left, right):
+        l, r = 0, 0
+        sorted_list = []
+        while l < len(left) and r < len(right):
+            if left[l] <= right[r]:
+                sorted_list.append(left[l])
+                l += 1
+            else:
+                sorted_list.append(right[r])
+                r += 1
+        sorted_list.extend(left[l:])
+        sorted_list.extend(right[r:])
+        return sorted_list
+
+    width = 1
+    n = len(arr)
+    if n < 1: return []
+    while width < n:
+        for i in range(0, n, 2*width):
+            left = arr[i:i+width]
+            right = arr[i+width:i+2*width]
+            arr[i:i+2*width] = merge(left, right)
+        width *= 2
+    return arr
--- test/part1/test_mergesort.py
+++ test/part1/test_mergesort.py
@@ -1,7 +1,11 @@
 import unittest
-from src.part1.mergesort import mergesort
+from src.part1.mergesort import mergesort, mergesort_iter
 
 class TestMergesort(unittest.TestCase):
     def test_mergesort(self):
         arr = [9, 3, 7, 4, 69, 420, 42]
         self.assertEqual(mergesort(arr), [3, 4, 7, 9, 42, 69, 420])
+
+    def test_mergesort_iter(self):
+        arr = [9, 3, 7, 4, 69, 420, 42]
+        self.assertEqual(mergesort_iter(arr), [3, 4, 7, 9, 42, 69, 420])
