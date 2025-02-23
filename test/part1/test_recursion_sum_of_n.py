9b14f623bbd37d988fea0e5e444a2a2d7aec4620
Daniel Blancas <edblancas@gmail.com>
2025-02-22 18:44:38 -0600

    add test for sum of n tabulation

--- test/part1/test_recursion_sum_of_n.py
+++ test/part1/test_recursion_sum_of_n.py
@@ -1,15 +1,18 @@
 import unittest
-from src.part1.recursion_sum_of_n import sum_of_n_rec, sum_of_n_tail_rec, sum_of_n_iter, sum_of_n_rec_log
+from src.part1.recursion_sum_of_n import sum_of_n_rec, sum_of_n_tail_rec, sum_of_n_iter, sum_of_n_rec_log, sum_of_n_tab
 
 class TestRecSumN(unittest.TestCase):
+    def test_sum_n_rec_log(self):
+        self.assertEqual(sum_of_n_rec_log(3), 6)
+
     def test_sum_n_rec(self):
         self.assertEqual(sum_of_n_rec(3), 6)
 
     def test_sum_n_tail_rec(self):
-        self.assertEqual(sum_of_n_tail_rec(3, 1), 6)
+        self.assertEqual(sum_of_n_tail_rec(3, 0), 6)
 
     def test_sum_n_iter(self):
-        self.assertEqual(sum_of_n_iter(3, 1), 6)
+        self.assertEqual(sum_of_n_iter(3, 0), 6)
 
-    def test_sum_n_rec_log(self):
-        self.assertEqual(sum_of_n_rec_log(3), 6)
+    def test_sum_of_n_tab(self):
+        self.assertEqual(sum_of_n_tab(3), 6)
